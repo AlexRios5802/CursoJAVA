f4818624908049f6c662b505f81c119b767b7e07
Alex Rios <al066668@uacam.mx>
2025-05-07 23:45:19 -0600

    Subo dos archivo, el de comprobación de contraseñas lo actualizaré, debo de comprobar si los cambios se ven

---
+++ src/CicloDowhile.java
@@ -0,0 +1,10 @@
+public class CicloDowhile {
+    public static void main(String[] args) {
+        System.out.println("*** Ciclo do-while ***");
+
+        var contador = 1;
+        do {
+            System.out.println(contador++);
+        } while (contador <= 3);
+    }
+}
