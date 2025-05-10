5f4a6d98e244d262f5e6ef48df8d12afd8bc2bf5
Alex Rios <al066668@uacam.mx>
2025-05-09 23:12:27 -0600

    Se le agregó un tiempo para cada número que se imprime

--- src/NumerosParesFor.java
+++ src/NumerosParesFor.java
@@ -1,10 +1,11 @@
 public class NumerosParesFor {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws InterruptedException {
         System.out.println("*** Números pares usando ciclo for ***");
 
         for (var i = 0; i <= 20; i++) {
             if (i % 2 == 0)
                 System.out.print(i + " ");
+                Thread.sleep(150);
         }
     }
 }
