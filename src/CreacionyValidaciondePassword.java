8668640e9e838506bb6263df166e7edb640a96f1
Alex Rios <al066668@uacam.mx>
2025-05-23 21:20:37 -0600

    El programa que hice ya tiene solucionado los bugs que tenía, las inpresiones infinitas de ingresar solo 6 caracteres, está solucionado. Ahora, en el programa del curso, este es más corto qué el que hice, necesito saber como porqué es más óptimo

--- src/CreacionyValidaciondePassword.java
+++ src/CreacionyValidaciondePassword.java
@@ -6,16 +6,16 @@ public class CreacionyValidaciondePassword {
         System.out.println("*** Creación y Validación de Contraseña ***");
 
         var consola = new Scanner(System.in);
-
-        System.out.print("Ingresa tu password: ");
-        var password = consola.nextLine();
-
         var salir = false;
 
         while (!salir) {
+            System.out.print("Ingresa tu password (6 caracteres): ");
+            var password = consola.nextLine();
+
             if (password.length() == 6) {
                 System.out.println("Guardando...\n");
                 Thread.sleep(1000);
+
                 System.out.print("""
                         -=-=-=-=-= Validación de contraseña =-=-=-=-=-
                         Ingresar contraseña:\s""");
@@ -26,16 +26,16 @@ public class CreacionyValidaciondePassword {
                 } else {
                     System.out.println("Contraseña incorrecta");
                 }
+
                 System.out.println("Fin del programa");
+                salir = true; // Finaliza el ciclo
 
             } else {
-                System.out.println("Deben ser solo 6 caracteres. Vuelve a escribir");
-
+                System.out.println("Deben ser solo 6 caracteres. Vuelve a escribir\n");
+                // El bucle continuará y pedirá nuevamente la contraseña
             }
-
         }
+
         System.out.println("FIN");
-
-
     }
 }
